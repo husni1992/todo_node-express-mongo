971d4574927b059192d79a58df6ae0e649f5f778
Husny Ahamed <mgha123@gmail.com>
2018-02-02 00:48:42 +0530

    Changed mongodb connection of local one

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://rihan_xyz:Abcd1234@ds123258.mlab.com:23258/todo_db");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = { mongoose };
