3f96aec1f5f23b87b7810c7ea98b6e8dd33c5426
Husny Ahamed <mgha123@gmail.com>
2018-02-01 22:29:56 +0530

    Added GET /todos/todoId

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -8,6 +9,18 @@ var { User } = require("./models/user");
 var app = express();
 app.use(bodyParser.json());
 
+app.get("/todos", (req, res) => {
+   Todo.find().then(
+      todos => {
+         res.send({ success: true, todos });
+      },
+      error => {
+         res.status(400).send({ success: false, error });
+      }
+   );
+});
+
+// GET /todos
 app.post("/todos", (req, res) => {
    console.log(req.body);
    var todo = new Todo({
@@ -24,16 +37,24 @@ app.post("/todos", (req, res) => {
    );
 });
 
-app.get("/todos", (req, res) => {
-   Todo.find().then(
-      todos => {
-         res.send({ success: true, todos });
-      },
-      error => {
-         res.status(400).send({ success: false, error });
-      }
-   );
+// GET /todos/2445323
+app.get("/todos/:todoId", (req, res) => {
+   const id = req.params.todoId;
+
+   if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+   }
+
+   Todo.findById(id)
+      .then(todo => {
+         if (!todo) {
+            return res.status(404).send();
+         }
+         res.send({ todo });
+      })
+      .catch(error => res.status(400).send({ error }));
 });
+
 app.listen(3000, () => {
    console.log("Starting on port 3000");
 });
