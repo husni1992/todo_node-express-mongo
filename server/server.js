06b3637f376d9222fac54fc3d32d4cc1c9475c33
Husny Ahamed <mgha123@gmail.com>
2018-02-01 17:41:36 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -8,22 +8,37 @@ var { User } = require("./models/user");
 var app = express();
 app.use(bodyParser.json());
 
-app.post("/todos", (req, res) => {
-   console.log(req.body);
-   var todo = new Todo({
-      text: req.body.text
-   });
+app
+   .post("/todos", (req, res) => {
+      console.log(req.body);
+      var todo = new Todo({
+         text: req.body.text
+      });
 
-   todo.save().then(
-      doc => {
-         res.send({ success: true, data: doc });
-      },
-      err => {
-         res.status(400).send({ success: false, error: err });
-      }
-   );
-});
+      todo.save().then(
+         doc => {
+            res.send(doc);
+         },
+         err => {
+            res.status(400).send({ success: false, error: err });
+         }
+      );
+   })
+
+   .get("/todos", (req, res) => {
+      Todo.find(
+         {},
+         (err, todos) => {
+            res.send({ success: true, data: todos });
+         },
+         err => {
+            res.status(400).send({ success: false, error: err });
+         }
+      );
+   });
 
 app.listen(3000, () => {
    console.log("Starting on port 3000");
 });
+
+module.exports = { app };
